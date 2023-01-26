8057c20299c56e622d596eeae7ac9ae790dcb5c1
OHOUD ALHEFEITI <36378728+El3en@users.noreply.github.com>
2023-01-26 22:54:15 +0400

    Update web.php

--- Task2/web.php
+++ Task2/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CategoriesController;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,23 +18,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/add-product', [HomeController::class, 'add_product']);
 Route::get('/shop', [HomeController::class, 'shop']);
-Route::get('/contact',[ContactController::class,'index'])->name('contact');
-Route::post('/contact',[ContactController::class,'sendMessage']);
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/admin', [AdminController::class, 'admin']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::prefix('/admin')->group(function () {
+    Route::get('categories', [CategoriesController::class, 'index']);
+    Route::post('categories', [CategoriesController::class, 'store'])->name('admin.categories');
+    Route::get('categories/create', [CategoriesController::class, 'create']);
+    Route::get('categories/{id}/edit', [CategoriesController::class, 'edit']);
+    Route::put('categories/{id}', [CategoriesController::class, 'update']);
+    Route::get('categories/{id}', [CategoriesController::class, 'show']);
+    Route::delete('categories/{id}', [CategoriesController::class, 'destroy']);
 
-require __DIR__ . '/auth.php';
-
-Route::middleware(['auth', 'can:is_admin'])->prefix('/admin')->group(function () {
     Route::resource('products', ProductsController::class);
-    Route::resource('categories', CategoriesController::class);
+    Route::get('products', [ProductsController::class, 'index']);
+    Route::post('products', [ProductsController::class, 'store'])->name('admin.products');
+    Route::get('categories/create', [CategoriesController::class, 'create']);
 });
